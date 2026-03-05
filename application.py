ec248a28aa262e6ec6cb9b33c045af0417b7c585
sijomonps <sijomon700@gmail.com>
2026-03-05 12:44:24 +0530

    port

--- application.py
+++ application.py
@@ -161,5 +161,5 @@ def health():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5005))
     application.run(debug=False, host="0.0.0.0", port=port)
